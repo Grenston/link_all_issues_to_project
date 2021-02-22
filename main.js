f3d66ba9fdf214d14521d57ac10ba6d7b5d30383
grenston <grenston.george@gmail.com>
2021-02-22 16:31:13 +0530

    started my code

---
+++ main.js
@@ -0,0 +1,24 @@
+exports.issues_add = async function(pat,repo) {
+    const { Octokit } = require("@octokit/rest");
+    const octokit = new Octokit({
+        auth: pat
+    });
+    const user = await octokit.request('GET /user')
+    const issues = await octokit.paginate('GET /repos/{owner}/{repo}/issues', {
+        owner: user.data.login,
+        repo: repo,
+        state: 'all'
+      });
+    for(issue of issues) {
+        if(!issue.pull_request) {
+            await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+                owner: user.data.login,
+                repo: repo,
+                issue_number: issue.number,
+                labels: ['SpaceGame']
+              })
+        }
+    }
+}
+const constants = require("./constants")
+this.issues_add(constants.personal_pat,constants.repo)
